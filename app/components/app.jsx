448c58ad3f7770555e470be9dda8e7a8c91359da
Matt Stuhring <whereyouatmatt@gmail.com>
2016-08-20 15:56:12 -0700

    create mock landing page

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,9 +1,121 @@
-import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import PersonAdd from 'material-ui/svg-icons/content/add-circle';
+import Help from 'material-ui/svg-icons/action/help';
+import Person from 'material-ui/svg-icons/action/account-circle';
+import FlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import LocalDining from 'material-ui/svg-icons/maps/restaurant';
+import { red700, green700, orange500, brown700 }
+  from 'material-ui/styles/colors';
+import Paper from 'material-ui/Paper';
+import {BottomNavigation, BottomNavigationItem}
+  from 'material-ui/BottomNavigation';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+import FontIcon from 'material-ui/FontIcon';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
 
 const App = React.createClass({
   render() {
-    return <RaisedButton label="Meal Wheel!" />;
+    const styleFlatButton = {
+      height: '64px',
+      lineHeight: '64px'
+    };
+
+    const styleTitle = {
+      cursor: 'pointer'
+    };
+
+    const styleIcon = {
+      largeIcon: {
+        width: 150,
+        height: 150,
+      },
+      large: {
+        width: 180,
+        height: 180,
+      },
+    };
+
+    const styleBottomNav = {
+      backgroundColor: '#f2df8f',
+      position: 'fixed',
+      bottom: 0
+    };
+
+    const style = {
+      height: 100,
+      width: '100%',
+      marginTop: 20,
+      textAlign: 'center',
+      display: 'inline-block',
+    };
+
+    return <div>
+      <AppBar
+        onTitleTouchTap={this.handleTitleTouchTap}
+        title="Meal Wheel"
+        titleStyle={styleTitle}
+      >
+        <FlatButton
+          style={styleFlatButton}
+          onTouchTap={this.handleTouchTap}
+          label="New Post"
+        />
+      </AppBar>
+
+      <div>
+        <div className="buttonBox">
+          <IconButton
+            tooltip="New User"
+            touch={true}
+            tooltipPosition="bottom-right"
+            iconStyle={styleIcon.largeIcon}
+            style={styleIcon.large}
+          >
+            <PersonAdd color={red700} />
+          </IconButton>
+
+          <IconButton
+            tooltip="Login"
+            touch={true}
+            tooltipPosition="bottom-right"
+            iconStyle={styleIcon.largeIcon}
+            style={styleIcon.large}
+          >
+            <Person color={green700} />
+          </IconButton>
+        </div>
+
+        <div className="buttonBox">
+          <IconButton
+            tooltip="What We Do"
+            touch={true}
+            tooltipPosition="bottom-right"
+            iconStyle={styleIcon.largeIcon}
+            style={styleIcon.large}
+          >
+            <Help color={orange500} />
+          </IconButton>
+
+          <IconButton
+            tooltip="Lets Go!"
+            touch={true}
+            tooltipPosition="bottom-right"
+            iconStyle={styleIcon.largeIcon}
+            style={styleIcon.large}
+          >
+            <LocalDining color={brown700} />
+          </IconButton>
+        </div>
+      </div>
+
+      <Paper zDepth={3}>
+        <BottomNavigation style={styleBottomNav} />
+      </Paper>
+
+    </div>;
   }
 });
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,26 @@
 import App from 'components/app';
+import { blue700 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  flatButton: {
+    buttonFilterColor: blue700,
+    textColor: 'white'
+  },
+
+  palette: {
+    primary1Color: '#f2df8f',
+  }
+});
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <App />
   </MuiThemeProvider>,
   document.getElementById('app')
