65ed25f297a0ae0bd3a34c149f9d1dbf8aa38672
John Carney <jcarney000@gmail.com>
2016-08-25 10:40:02 -0700

    linted index

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,9 @@
-import Routes from 'components/Routes';
-import { blue700 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from 'components/Routes';
+import { blue700 } from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
