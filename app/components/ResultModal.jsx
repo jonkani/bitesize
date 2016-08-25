03872a325c5f23969575bb0d9280078e3b53fd1d
John Carney <jcarney000@gmail.com>
2016-08-25 13:12:57 -0700

    linted result modal

--- app/components/ResultModal.jsx
+++ app/components/ResultModal.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dissatisfied from 'material-ui/svg-icons/social/sentiment-dissatisfied';
-import { red700, green700, yellow600, brown700 }
-  from 'material-ui/styles/colors';
-
+import React from 'react';
+import { red700 } from 'material-ui/styles/colors';
 
 const ResultModal = React.createClass({
   handleClose() {
@@ -14,14 +10,14 @@ const ResultModal = React.createClass({
   },
 
   render() {
-    const restaurant = this.props.modalData.restaurant
+    const restaurant = this.props.modalData.restaurant;
     const styleDialog = {
       width: '100%',
-      maxWidth: 'none',
+      maxWidth: 'none'
     };
 
     const styleBody = {
-      padding: '20px',
+      padding: '20px'
     };
 
     const actions =
@@ -29,16 +25,24 @@ const ResultModal = React.createClass({
         <FlatButton
           label="Close"
           onTouchTap={this.handleClose}
-          style={{backgroundColor: red700, display: 'inline-block', marginBottom: '5px'}}
+          style={{
+            backgroundColor: red700,
+            display: 'inline-block',
+            marginBottom: '5px'
+          }}
         />
-        <div style={{display: 'inline-block', }}>
-          <a href={restaurant.url}><img style={{width: '75px'}} src="./images/yelp.jpg"></img></a>
+        <div style={{ display: 'inline-block' }}>
+          <a href={restaurant.url}>
+            <img src="./images/yelp.jpg" style={{ width: '75px' }} />
+          </a>
           <p
             style={{
               marginLeft: '80px',
               marginTop: '0px',
               marginBottom: '0px'
-            }}>Rating: {restaurant.rating} / Reviews: {restaurant.reviewCount}
+            }}
+          >
+            Rating: {restaurant.rating} / Reviews: {restaurant.reviewCount}
           </p>
 
         </div>
@@ -48,36 +52,39 @@ const ResultModal = React.createClass({
       <div>
         <Dialog
           actions={actions}
-          contentStyle={styleDialog}
           bodyStyle={styleBody}
+          contentStyle={styleDialog}
           modal={false}
-          open={this.props.modalData.open}
           onRequestClose={this.handleClose}
+          open={this.props.modalData.open}
         >
-        <div>
-          <Paper
-            style={{
-              display: 'inline-block',
-              width: '130px',
-              height: '130px'
-            }}>
+          <div>
+            <Paper
+              style={{
+                display: 'inline-block',
+                width: '130px',
+                height: '130px'
+              }}
+            >
               <img
-                style={{width: '130px', height: '130px'}} src={restaurant.imageUrl}
+                src={restaurant.imageUrl}
+                style={{ width: '130px', height: '130px' }}
               />
-          </Paper>
+            </Paper>
 
-          <div style={{display: 'inline-block', marginLeft: '15px'}}>
-            <div className="resultLink">
-              <a href={restaurant.url}>{restaurant.name}</a>
+            <div style={{ display: 'inline-block', marginLeft: '15px' }}>
+              <div className="resultLink">
+                <a href={restaurant.url}>{restaurant.name}</a>
+              </div>
+              {restaurant.location ? restaurant.location.map(
+                (element, index) => {
+                  return <h5 className="resultForm" key={index}>
+                  {restaurant.location[index]}
+                  </h5>;
+                }) : <h5 />}
+              <h6 className="resultPhone">{restaurant.displayPhone}</h6>
             </div>
-            {restaurant.location ? restaurant.location.map((element, index) => {
-              return <h5 className="resultForm" key={index + 'i'}>
-                {restaurant.location[index]}
-              </h5>
-            }) : <h5></h5>}
-            <h6 className="resultPhone">{restaurant.displayPhone}</h6>
           </div>
-        </div>
           <p>{restaurant.snippetText}</p>
         </Dialog>
       </div>
